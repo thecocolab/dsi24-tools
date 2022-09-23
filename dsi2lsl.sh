eb5cdd9170cb84e4e77518d846a14856c2ae8528
Philipp Thölke <philipp.thoelke@posteo.de>
2022-09-23 14:09:54 -0400

    Fix lib path

--- dsi2lsl.sh
+++ dsi2lsl.sh
@@ -3,7 +3,7 @@
 if [ -d dsi24_lib ]
 then
 	# run dsi2lsl program
-	sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH:lib ./dsi24_lib/dsi2lsl $@
+	sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH:dsi24_lib ./dsi24_lib/dsi2lsl $@
 else
 	# setup was not completed
 	echo "Please run setup.sh first"
