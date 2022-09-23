781ccf9410da40ea2c909ec64b15edba03c96f8d
Philipp Thölke <philipp.thoelke@posteo.de>
2022-09-23 14:33:29 -0400

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
-# TODO: add Python dependencies
+
+with open("requirements.txt") as f:
+    dependencies = f.read().splitlines()
+
 setup(
     name="dsi24-lib",
     version="0.0.1",
     packages=find_packages("dsi24_lib"),
+    install_requires=dependencies,
 )
