63ecda9b01dbbd2bfa1fa90ac34066c455d55745
Philipp Thölke <philipp.thoelke@posteo.de>
2022-12-19 18:49:00 -0500

    Update beta feature

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -84,7 +84,7 @@ def lzc(raw, freq, amp):
 
 features = {
     "alpha": partial(psd, ch_select=["O1", "O2"], lower=8, upper=12),
-    "beta": partial(psd, ch_select=["C3", "C4"], lower=12, upper=30),
+    "beta": partial(psd, ch_select=["C3", "C4"], lower=13, upper=20),
     "lempel-ziv": partial(lzc, ch_select=["Fp1", "Fp2"]),
 }
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
